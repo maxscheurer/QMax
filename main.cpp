33d768923b28d273509d0319dbe8f726aca6e8f5
Max Scheurer <max.scheurer@me.com>
2016-12-16 17:17:58 +0100

    cleaned

--- main.cpp
+++ main.cpp
@@ -55,45 +55,6 @@ SmallMatrix<double> computeOneBodyIntegrals(Operator op, BasisSet basisSet, cons
     return S;
 }
 
-void overlap(BasisSet basisSet, const vector<Atom> atoms) {
-    Engine s_engine(Operator::overlap,  // will compute overlap ints
-                    basisSet.max_nprim(),    // max # of primitives in shells this engine will accept
-                    basisSet.max_l()         // max angular momentum of shells this engine will accept
-    );
-    s_engine.set_params(make_point_charges(atoms));
-    const auto& res = s_engine.results();
-    auto shell2bf = basisSet.shell2bf();
-    double** overlapMatrix = new double*[7];
-    for (int m = 0; m < 7; ++m) {
-        overlapMatrix[m] = new double[7];
-    }
-
-    for (int i = 0; i != basisSet.size(); ++i) {
-        for (int k = 0; k != basisSet.size() ; ++k) {
-            s_engine.compute(basisSet[i], basisSet[k]);
-            auto shellSizeI = basisSet[i].size();
-            auto shellSizeK = basisSet[k].size();
-            auto bf1 = shell2bf[i];
-            auto bf2 = shell2bf[k];
-            auto integral_shell = res[0];
-            for (auto j = 0; j < shellSizeI; ++j) {
-                for (auto l = 0; l < shellSizeK; ++l) {
-//                    cout << j+bf1 << " " << l+bf2 << " " << integral_shell[j*shellSizeK+l] << endl;
-                    overlapMatrix[j+bf1][l+bf2] = integral_shell[j*shellSizeK+l];
-                }
-            }
-        }
-    }
-
-
-    for (int n = 0; n < 7; ++n) {
-        for (int i = 0; i < 7; ++i) {
-            cout << overlapMatrix[n][i] << " ";
-        }
-        cout << endl;
-    }
-}
-
 int main() {
     libint2::initialize();
 
