ecfffdbe4390fb1dfff3b4bcc000eb5dcb8e4609
Max Scheurer <max.scheurer@me.com>
2016-12-17 09:32:45 +0100

    initial SCF guess

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@
 #include <string>
 #include <vector>
 #include <linalgwrap/SmallMatrix.hh>
+#include <linalgwrap/SmallVector.hh>
+#include <linalgwrap/eigensystem.hh>
 
 using libint2::Shell;
 using libint2::Engine;
@@ -55,6 +57,22 @@ SmallMatrix<double> computeOneBodyIntegrals(Operator op, BasisSet basisSet, cons
     return S;
 }
 
+double computeNuclearRepulsionEnergy(vector<Atom> atoms) {
+    double nrep = 0.0;
+    size_t natoms = atoms.size();
+    for (int i = 0; i < natoms; ++i) {
+        for (int j = i+1; j < natoms; ++j) {
+            auto xij = atoms[i].x - atoms[j].x;
+            auto yij = atoms[i].y - atoms[j].y;
+            auto zij = atoms[i].z - atoms[j].z;
+            auto r2 = xij * xij + yij * yij + zij * zij;
+            auto r = sqrt(r2);
+            nrep += atoms[i].atomic_number * atoms[j].atomic_number / r;
+        }
+    }
+    return nrep;
+}
+
 int main() {
     libint2::initialize();
 
@@ -66,13 +84,84 @@ int main() {
 //    assign a basis set & print number of BF
     BasisSet basisSet("STO-3G", atoms);
     cout << "Number of basis functions: " << basisSet.nbf() << endl;
+    int nbf = basisSet.nbf();
+    int nshells = basisSet.size();
+
+//    count number of electrons
+    auto nelectron = 0;
+    for (auto i = 0; i < atoms.size(); ++i)
+        nelectron += atoms[i].atomic_number;
+    const auto ndocc = nelectron / 2;
+
+//    compute the nuclear repulsion energy
+    double nrep = computeNuclearRepulsionEnergy(atoms);
+
+//    compute the basis set overlap
+    SmallMatrix<double> S = computeOneBodyIntegrals(Operator::overlap, basisSet,atoms);
 
 //    compute the core Hamiltonian
-    SmallMatrix<double> S = computeOneBodyIntegrals(Operator::overlap, basisSet,atoms);
-    SmallMatrix<double> NRep = computeOneBodyIntegrals(Operator::nuclear, basisSet,atoms);
-    cout << endl << endl;
-    cout << S << endl;
-    cout << NRep << endl;
+    SmallMatrix<double> NAttr = computeOneBodyIntegrals(Operator::nuclear, basisSet,atoms);
+    SmallMatrix<double> T = computeOneBodyIntegrals(Operator::kinetic, basisSet,atoms);
+//    cout << endl << endl;
+//    cout << S << endl;
+//    cout << NAttr << endl;
+    cout << "Nuclear repulsion energy is: " << nrep << endl;
+
+    SmallMatrix<double> HCore = T + NAttr;
+    cout << "Core Hamiltonian: " << endl << HCore << endl;
+
+    bool converged = false;
+    int scfiteration = 0;
+
+    SmallMatrix<double> F(nbf,nbf);
+    SmallMatrix<double> D(nbf,nbf);
+    double escf = 0.0;
+    while (!converged) {
+        cout << "SCF iteration: " << scfiteration << endl;
+        if (scfiteration == 0) {
+            F = HCore;
+            auto sol = eigensystem_hermitian(F, S);
+            auto evals = sol.evalues();
+            auto evecs = sol.evectors();
+            auto v=evecs.subview(krims::range((unsigned  long) ndocc));
+//            cout << evals[0] << endl;
+//            cout << evecs << endl;
+            D = outer_prod_sum(v,v);
+            cout << "Density matrix" << endl << D << endl;
+            SmallMatrix<double> EEl = D*(HCore+F);
+            cout << EEl << endl;
+            double elEnergy = 0.0;
+            for (size_t col = 0; col < EEl.n_cols(); ++col) {
+                for (size_t row = 0; row < EEl.n_rows(); ++row) {
+                    elEnergy += EEl(row, col);
+                }
+            }
+            escf = elEnergy + nrep;
+            cout << "Initial electronic energy: " << elEnergy << endl;
+            cout << "Initial SCF energy: " << escf << endl;
+
+            SmallMatrix<double> FNew(nbf,nbf);
+            for (int i = 0; i < nshells; ++i) {
+                for (int j = 0; j < nshells; ++j) {
+                    FNew(i,j)=HCore(i,j);
+                    for (int k = 0; k < nshells; ++k) {
+                        for (int l = 0; l < nshells; ++l) {
+//                            FNew(i,j) += D(i,j)*
+                        }
+                    }
+                }
+            }
+
+        } else {
+
+
+
+
+            converged = true;
+        }
+        ++scfiteration;
+    }
+
 //    don't use libint after this!
     libint2::finalize();
     return 0;
